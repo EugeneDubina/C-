a3c5daf7e80969b6e67d007cec5777a349271581
EugeneDubina <dubina.evgenij@gmail.com>
2022-12-12 14:27:00 +0300

    addTask_19

---
+++ task_19/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Console.WriteLine("Enter number: ");
+int num = int.Parse(Console.ReadLine());
+int numRevers = 0;
+
+if(num < 0 || (num % 10 == 0 && num != 0))
+{
+    Console.WriteLine("No!");
+    return;
+}
+while (num > numRevers)
+{
+    numRevers = numRevers * 10 + num % 10;
+    num /= 10;
+}
+if (num == numRevers || num == numRevers/10)
+{
+    Console.WriteLine("ДА");
+}
+else 
+{
+    Console.WriteLine("Нет");
+}
