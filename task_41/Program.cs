94f5a93859b848759d6dd10f385b5c5687cd2d0b
EugeneDubina <dubina.evgenij@gmail.com>
2022-12-16 14:45:53 +0300

    addHM6

---
+++ task_41/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Кол-во вводимых чисел");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Введите {n} чисел(-а) через Enter" );
+
+int count = 0;
+for (int i=0; i<n; i++)
+{
+if (int.Parse(Console.ReadLine()) > 0)
+    count=count+1;
+}
+Console.WriteLine($" Кол-во положительных чисел = {count}");
---
+++ task_43/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Значение b1.  b1 = ");
+double b1 = double.Parse(Console.ReadLine());
+
+Console.Write("Значение k1.  k1 = ");
+double k1 = double.Parse(Console.ReadLine());
+
+Console.Write("Значение b2.  b2 = ");
+double b2 = double.Parse(Console.ReadLine());
+
+Console.Write("Значение k2.  k2 = ");
+double k2 = double.Parse(Console.ReadLine());
+
+double x = (b2-b1)/(k1-k2);
+double y = k1*x+b1;
+
+Console.WriteLine($"Точка пересечения - ({x},{y})");
